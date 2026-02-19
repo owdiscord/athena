ca22eaf3961c95c8a7476aa8ed360167794ae5ca
graphiteisaac <isaac@grphcrtv.com>
2026-02-19 13:05:44 +1100

    fix up vite resolution for dashboard

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwind from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 export default defineConfig((configEnv) => {
   return {
+    resolve: {
+      alias: [
+        {
+          find: /^@athena\/shared\/(.*)$/,
+          replacement: fileURLToPath(
+            new URL("../shared/src/$1.ts", import.meta.url),
+          ),
+        },
+      ],
+    },
     server: {
       port: 3002,
       host: "0.0.0.0",
